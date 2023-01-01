bed748dc14eb97c5745aea72d5f0dc7cf646d04f
Jorb Dehmel <jdehmel@outlook.com>
2022-12-31 19:54:01 -0700

    Implemented loadModel

--- jgame3d/files.cpp
+++ jgame3d/files.cpp
@@ -24,14 +24,16 @@ bool saveModel(const Model &what, const char *where)
     
     for (auto polygon : what.polygons)
     {
-        file << polygon.points.size() << ", "
-             << polygon.color.r << ", " << polygon.color.g << ", "
-             << polygon.color.b << ", " << polygon.color.a << '\n';
+        file << polygon.points.size() << " "
+             << polygon.color.r << " " << polygon.color.g << " "
+             << polygon.color.b << " " << polygon.color.a << '\n';
         
         for (auto point : polygon.points)
         {
-            file << point.x << ", " << point.y << ", " << point.z << ", ";
+            file << point.x << " " << point.y << " " << point.z << " ";
         }
+
+        file << "\n";
     }
 
     file.close();
@@ -40,5 +42,30 @@ bool saveModel(const Model &what, const char *where)
 
 Model loadModel(const char *where)
 {
-   // Todo: implement this
+    // Todo: implement this
+    ofstream file(where);
+    if (!file.open())
+        return false;
+    
+    Model out;
+    while (!file.eof())
+    {
+        int num;
+        Polygon poly;
+
+        file >> num;
+        file >> poly.color.r >> poly.color.g >> poly.color.b >> poly.color.a;
+
+        for (int i = 0; i < num; i++)
+        {
+            Point3D point;
+            file >> point.x >> point.y >> point.z;
+            poly.push_back(point);
+        }
+
+        out.polygons.push_back(poly);
+    }
+    file.close();
+
+    return out;
 }
